66c4c569599ff989326c90df3964b1d05ebfe951
xArthurDev <arthurrguimaraes12@gmail.com>
2021-11-19 16:14:34 -0300

    fix: add missing columns at entity

--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -3,15 +3,21 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    name: string;
+  @Column()
+  username: string;
 
-    @Column()
-    birthDate: string;
+  @Column()
+  password: string;
 
-    @Column()
-    photo: string;
+  @Column()
+  name: string;
+
+  @Column()
+  birthDate: string;
+
+  @Column()
+  photo: string;
 }
