5b702c2a16f6b16985d950858cbe45c6e4305fe8
xArthurDev <arthurrguimaraes12@gmail.com>
2021-11-19 16:16:18 -0300

    feat: create auth service

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,33 @@
+/* eslint-disable prettier/prettier */
+import { UsersService } from './../users/users.service';
+
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string) {
+    const user = await this.usersService.getUserDetailsByUsername(username);
+
+    const isPasswordMatch = await bcrypt.compare(pass, user.password)
+    if (user && isPasswordMatch) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    throw new HttpException('Incorrect password or username', HttpStatus.UNAUTHORIZED);
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
