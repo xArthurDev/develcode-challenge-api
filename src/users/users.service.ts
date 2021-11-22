a1c7e8a150163c6e08bd7a4e8f3b4aeee072ad57
xArthurDev <arthurrguimaraes12@gmail.com>
2021-11-22 10:45:43 -0300

    fix: return of users functions was returning password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,1 +1,56 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/ban-types */
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult, DeleteResult } from 'typeorm';
+import { User } from './users.entity';
+import * as bcrypt from 'bcrypt'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  async getAllUsers(): Promise<any[]> {
+    const users = await this.usersRepository.find();
+    return users.map(({ password, ...content }) => content)
+  }
+
+  async createUser(user: User): Promise<Object> {
+    const userId = Math.floor(Math.random() * 10000);
+    const hashedPassword = await bcrypt.hash(user.password, 10)
+    user = {
+      ...user,
+      id: userId,
+      password: hashedPassword
+    };
+    await this.usersRepository.save(user);
+    const { password, ...content } = user;
+    return content;
+  }
+
+  async updateUser(user: any): Promise<UpdateResult> {
+    user = await this.usersRepository.update(user.id, user);
+    const { password, ...content } = user;
+    return content;
+  }
+
+  async deleteUser(id: string): Promise<DeleteResult> {
+    return await this.usersRepository.delete(id);
+  }
+
+  async getUserDetailsByUsername(username: string): Promise<any | undefined> {
+    const user = await this.usersRepository.findOne({ username });
+    const { password, ...content } = user;
+    return content;
+  }
+
+  async getUserDetails(id: string) {
+    const user = await this.usersRepository.findOne(id)
+    const { password, ...content } = user;
+    return content;
+  }
+}
